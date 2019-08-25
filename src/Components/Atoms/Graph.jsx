c5c135725c48bbd850c6afd072e24a8df406917e
Raul Castellanos <raulcastellanosh@hotmail.com>
2019-08-25 03:04:42 -0500

    added graph component

---
+++ src/Components/Atoms/Graph.jsx
@@ -0,0 +1,27 @@
+import React, { Component} from 'react';
+import {XYPlot, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, LineMarkSeries} from 'react-vis';
+
+// npm install react-vis
+
+const Graph = ({data1, data2, data3, color1, color2, color3}) => (
+    <XYPlot height={400} width= {400} xType="ordinal">
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineMarkSeries curve={'curveMonotoneX'} 
+          lineStyle={{stroke: color1}}
+          markStyle={{stroke: color1, fill: color1}}
+          data={data1} />
+          <LineMarkSeries curve={'curveMonotoneX'} 
+          lineStyle={{stroke: color2}}
+          markStyle={{stroke: color2, fill: color2}}
+          data={data2} />
+          <LineMarkSeries curve={'curveMonotoneX'} 
+          lineStyle={{stroke: color3}}
+          markStyle={{stroke: color3, fill: color3}}
+          data={data3} />
+        </XYPlot> 
+);
+
+export default Graph;
